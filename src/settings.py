f8e888538e017518ea181327d950b28387a374e2
bulldra <bullda@gmail.com>
2021-05-03 12:04:05 +0900

    テキストリスト対応

--- src/settings.py
+++ src/settings.py
@@ -13,9 +13,9 @@ def load_text_list(path):
     result = []
     path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
     with open(path, 'r') as conf:
-        for line in conf:
+        for line in conf.read().splitlines():
             result.append(line)
-    return line
+    return result
 
 settings_dict = load_json_setting('../config/settings.json')
 secrets_dict = load_json_setting('../config/secrets.json')
