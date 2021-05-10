6e81d33e4d670e62c0bfdc737d2bf2aebe63dc93
bulldra <bullda@gmail.com>
2021-05-10 17:20:54 +0900

    pandas対応・logzeroバージョン指定

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
 __version__ = "0.1.0"
 
-import pytest
 import main
+import settings
+import pandas
+import logzero
+
+logzero.logfile(
+    settings.settings_dict['logfile'],
+    loglevel=20,
+    maxBytes=1e6,
+    backupCount=3
+)
+logger = logzero.logger
 
 def test_answer():
     assert main.Main().execute() == True
+
+def test_pandas():
+    df = pandas.read_csv('../work/pcr_positive_daily.csv')
+    logger.info(df.shape)
+    logger.info(df.columns)
+    logger.info(df.dtypes)
+
+
+
