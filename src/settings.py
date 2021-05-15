a65ae821df65ce2fa9f985aa77c76ebc7aa76f2f
bulldra <bullda@gmail.com>
2021-05-15 23:43:56 +0900

    method名簡略化

--- src/settings.py
+++ src/settings.py
@@ -4,24 +4,28 @@ __version__ = "0.1.0"
 import os
 import json
 
-def load_json_setting(path):
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    if os.path.exists(path):
+def load_json(path):
+    path = _build_path(path)
+    if path is not None:
         with open(path, 'r') as conf:
             return json.load(conf)
     else:
-        return None
+        return {}
 
-def load_text_list(path):
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
-    if os.path.exists(path):
+def load_text(path):
+    path = _build_path(path)
+    if path is not None:
         result = []
         with open(path, 'r') as conf:
             for line in conf.read().splitlines():
                 result.append(line)
             return result
     else:
-        return None
+        return []
 
-settings_dict = load_json_setting('../config/settings.json')
-# secrets_dict = load_json_setting('../config/secrets.json')
+def _build_path(path):
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+    return path if os.path.exists(path) else None
+
+settings_dict = load_json('../config/settings.json')
+# secrets_dict = load_json('../config/secrets.json')
