0fe5ac5e5e248d9ab9b43604d365ebd47c85fad6
bulldra <bullda@gmail.com>
2021-05-15 23:44:31 +0900

    test拡充

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,14 +3,27 @@ __version__ = "0.1.0"
 
 import main
 import settings
-import pandas
-import logzero
-
-logzero.logfile(
-    settings.settings_dict['logfile'],
-    loglevel=20, maxBytes=1e6, backupCount=3
-)
-logger = logzero.logger
 
 def test_answer():
     assert main.Main().execute() == True
+
+def test_config():
+    assert settings.settings_dict['logfile'] == '../log/info.log'
+
+def test_load_json():
+    settings_dict = settings.load_json('../config/settings.json')
+    assert settings_dict['logfile'] == '../log/info.log'
+
+def test_load_text():
+    settings_list = settings.load_text('../config/settings.json')
+    assert settings_list[0] == '{'
+    assert settings_list[1] == '    "logfile" : "../log/info.log"'
+    assert settings_list[2] == '}'
+
+def test_notfilepath_list():
+    settings_list = settings.load_text('../config/settings.jso')
+    assert settings_list == []
+
+def test_notfilepath_json():
+    settings_dict = settings.load_json('../config/settings.jso')
+    assert settings_dict == {}
